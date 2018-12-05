2dfc87779b0d7d9274ef587bb7dee36f45117c67
Shyam Prasad Sitaula <shyam.sitaula@oit.com.np>
2018-12-05 14:16:35 +0545

    Initial print system done

--- resources/views/remittance/print-rmt.blade.php
+++ resources/views/remittance/print-rmt.blade.php
@@ -11,7 +11,7 @@
       }
 
       .top-name {
-        margin-left: 5in;
+        margin-left: 3in;
       }
 
       .top-spacer {
@@ -21,11 +21,12 @@
       p.rmt-footer {
         position: fixed;
         /* bottom: 2; */
+	  top: 4.5in;
       }
 
       .rmt-footer-fc {
         position: fixed;
-	left: 2in;
+	left: 1in;
       }
 
       .rmt-footer-date {
@@ -35,18 +36,18 @@
 
       .rmt-footer-sn {
         position: fixed;
-	left: 6in;
+	left: 8in;
       }
 
       .rmt-footer-total {
         position: fixed;
-	left: 8in;
+	left:10in;
       }
 
       @page {
         /* Page setup */
-        size: 15in 6in;
-	margin: 0mm;
+        size: A3;
+	margin: 0;
       }
 
       @media print {
@@ -61,17 +62,122 @@
         }
 
         .top-name {
-          margin-left: 5in;
+          margin-left: 2.5in;
         }
 
         .top-spacer {
           margin-top: 2.5in;
         }
 
+        .top-spacer-b {
+          margin-top: 2.35in;
+        }
+
         p.rmt-footer {
           position: fixed;
-          bottom: 2;
+	  top: 4.8in;
+          /*bottom: 2;*/
+	  page-break-inside: avoid;
         }
+
+        .rmt-footer-fc {
+          position: fixed;
+          left: 1in;
+        }
+
+        .rmt-footer-date {
+          position: fixed;
+          left: 3.5in;
+        }
+
+        .rmt-footer-sn {
+          position: fixed;
+          left: 8.2in;
+        }
+
+        .rmt-footer-total {
+          position: fixed;
+          left: 10.5in;
+        }
+
+	.page-breaker {
+	  page-break-before: always;
+	  margin: 0;
+	  padding: 0;
+	}
+
+	/* Remittance line numbers */
+	.rl-swastyayani {
+	    position: absolute;
+	    left: 2.8in;
+	}
+	.rl-istavrity {
+	    position: absolute;
+	    left: 3.8in;
+	}
+	.rl-acharyavrity {
+	    position: absolute;
+	    left: 4.5in;
+	}
+	.rl-dakshina {
+	    position: absolute;
+	    left: 5.1in;
+	}
+	.rl-sangathani {
+	    position: absolute;
+	    left: 5.65in;
+	}
+	.rl-ananda_bazar {
+	    position: absolute;
+	    left: 6.3in;
+	}
+	.rl-pranami {
+	    position: absolute;
+	    left: 7.05in;
+	}
+	.rl-swastyayani_awasista {
+	    position: absolute;
+	    left: 7.5in;
+	}
+	.rl-ritwiki {
+	    position: absolute;
+	    left: 8.3in;
+	}
+	.rl-utsav {
+	    position: absolute;
+	    left: 8in;
+	}
+	.rl-diksha_pranami {
+	    position: absolute;
+	    left: 8.75in;
+	}
+	.rl-acharya_pranami {
+	    position: absolute;
+	    left: 9in;
+	}
+	.rl-parivrity {
+	    position: absolute;
+	    left: 9.5in;
+	}
+	.rl-misc {
+	    position: absolute;
+	    left: 10in;
+	}
+
+	.rl-num {
+	  font-size: 13px;
+	}
+
+	.rl-p {
+	    position: relative;
+	    margin-top: 0;
+	    margin-bottom: 0;
+	    page-break-inside: avoid;
+	}
+
+	.rl-name {
+	  font-size: 14px;
+	}
       }
     </style>
   </head>
@@ -92,56 +198,76 @@
     </p>
 
     <!-- Remit lines -->
-    <p>
-      <table class="table">
-        <thead>
-        </thead>
-        <tbody>
-          @foreach ( $remittance->remittance_lines as $remittance_line)
-	    @if ($loop->index > 0 && $loop->index % 6 == 0)
-	        <tr class="new-page">
-		  <td>
-	            <div class="top-spacer">
-                      &nbsp;&nbsp;
-	            </div>
-		  </td>
-	        </tr>
-	    @endif
-            <tr>
-              <td>
-	        <div class="rl-name">
-                  {{ $remittance_line->oblate->person->first_name }}
-                  @if ($remittance_line->oblate->person->middle_name)
-                    {{ $remittance_line->oblate->person->middle_name }}
-                  @endif
-                  {{ $remittance_line->oblate->person->last_name }}
-                  /*
-                  {{ $remittance_line->oblate->worker->person->first_name }}
-                  @if ($remittance_line->oblate->worker->person->middle_name)
-                    {{ $remittance_line->oblate->worker->person->middle_name }}
-                  @endif
-                  {{ $remittance_line->oblate->worker->person->last_name }}
-	        </div>
-              </td>
-              <td class="nwo-prt-num-td">{{ $remittance_line->swastyayani }}</td>
-              <td class="nwo-prt-num-td">{{ $remittance_line->istavrity }}</td>
-              <td class="nwo-prt-num-td">{{ $remittance_line->acharyavrity }}</td>
-              <td class="nwo-prt-num-td">{{ $remittance_line->dakshina }}</td>
-              <td class="nwo-prt-num-td">{{ $remittance_line->sangathani }}</td>
-              <td class="nwo-prt-num-td">{{ $remittance_line->ritwiki }}</td>
-              <td class="nwo-prt-num-td">{{ $remittance_line->pranami }}</td>
-              <td class="nwo-prt-num-td">{{ $remittance_line->swastyayani_awasista }}</td>
-              <td class="nwo-prt-num-td">{{ $remittance_line->ananda_bazar }}</td>
-              <td class="nwo-prt-num-td">{{ $remittance_line->parivrity }}</td>
-              <td class="nwo-prt-num-td">{{ $remittance_line->misc }}</td>
-              <td class="nwo-prt-num-td">{{ $remittance_line->utsav }}</td>
-              <td class="nwo-prt-num-td">{{ $remittance_line->diksha_pranami }}</td>
-              <td class="nwo-prt-num-td">{{ $remittance_line->acharya_pranami }}</td>
-            </tr>
-          @endforeach
-        </tbody>
-      </table>
-    </p>
+    @foreach ( $remittance->remittance_lines as $remittance_line)
+      @if ($loop->index > 0 && $loop->index % 6 == 0)
+        <p class="page-breaker">
+	  &nbsp;&nbsp;
+        </p>
+        <p class="top-spacer-b">
+        </p>
+      @endif
+      <p class="rl-p">
+	<span class="rl-name">
+          {{ $remittance_line->oblate->person->first_name }}
+          @if ($remittance_line->oblate->person->middle_name)
+            {{ $remittance_line->oblate->person->middle_name }}
+          @endif
+          {{ $remittance_line->oblate->person->last_name }}
+	</span>
+
+	<span class="rl-num rl-swastyayani">
+          {{ $remittance_line->swastyayani }}
+	</span>
+	<span class="rl-num rl-istavrity">
+          {{ $remittance_line->istavrity }}
+	</span>
+	<span class="rl-num rl-acharyavrity">
+          {{ $remittance_line->acharyavrity }}
+	</span>
+	<span class="rl-num rl-dakshina">
+          {{ $remittance_line->dakshina }}
+	</span>
+	<span class="rl-num rl-sangathani">
+          {{ $remittance_line->sangathani }}
+	</span>
+	<span class="rl-num rl-ananda_bazar">
+          {{ $remittance_line->ananda_bazar }}
+	</span>
+	<span class="rl-num rl-pranami">
+          {{ $remittance_line->pranami }}
+	</span>
+	<span class="rl-num rl-swastyayani_awasista">
+          {{ $remittance_line->swastyayani_awasista }}
+	</span>
+	<span class="rl-num rl-ritwiki">
+          {{ $remittance_line->ritwiki }}
+	</span>
+	<span class="rl-num rl-utsav">
+          {{ $remittance_line->utsav }}
+	</span>
+	<span class="rl-num rl-diksha_pranami">
+          {{ $remittance_line->diksha_pranami }}
+	</span>
+	<span class="rl-num rl-acharya_pranami">
+          {{ $remittance_line->acharya_pranami }}
+	</span>
+	<span class="rl-num rl-parivrity">
+          {{ $remittance_line->parivrity }}
+	</span>
+	<span class="rl-num rl-misc">
+          {{ $remittance_line->misc }}
+	</span>
+      </p>
+      <p class="rl-p">
+	<span class="rl-name">
+          *{{ $remittance_line->oblate->worker->person->first_name }}
+          @if ($remittance_line->oblate->worker->person->middle_name)
+            {{ $remittance_line->oblate->worker->person->middle_name }}
+          @endif
+          {{ $remittance_line->oblate->worker->person->last_name }}
+	</span>
+      </p>
+    @endforeach
    
     <!-- Footer -->
     <p class="rmt-footer">
--- resources/views/remittance/show-rmt.blade.php
+++ resources/views/remittance/show-rmt.blade.php
@@ -43,15 +43,15 @@
 	        <th>Acharyavrity</th>
 	        <th>Dakshina</th>
 	        <th>Sangathani</th>
-	        <th>Ritwiki</th>
+	        <th>Ananda bazar</th>
 	        <th>Pranami</th>
 	        <th>Swa Awa</th>
-	        <th>Ananda bazar</th>
-	        <th>Parivrity</th>
-	        <th>Misc</th>
+	        <th>Ritwiki</th>
 	        <th>Utsav</th>
 	        <th>Diksha Pranami</th>
 	        <th>Acharya Pranami</th>
+	        <th>Parivrity</th>
+	        <th>Misc</th>
 	        </tr>
 	      </thead>
 	      <tbody>
@@ -72,15 +72,15 @@
 		    <td>{{ $remittance_line->acharyavrity }}</td>
 		    <td>{{ $remittance_line->dakshina }}</td>
 		    <td>{{ $remittance_line->sangathani }}</td>
-		    <td>{{ $remittance_line->ritwiki }}</td>
+		    <td>{{ $remittance_line->ananda_bazar }}</td>
 		    <td>{{ $remittance_line->pranami }}</td>
 		    <td>{{ $remittance_line->swastyayani_awasista }}</td>
-		    <td>{{ $remittance_line->ananda_bazar }}</td>
-		    <td>{{ $remittance_line->parivrity }}</td>
-		    <td>{{ $remittance_line->misc }}</td>
+		    <td>{{ $remittance_line->ritwiki }}</td>
 		    <td>{{ $remittance_line->utsav }}</td>
 		    <td>{{ $remittance_line->diksha_pranami }}</td>
 		    <td>{{ $remittance_line->acharya_pranami }}</td>
+		    <td>{{ $remittance_line->parivrity }}</td>
+		    <td>{{ $remittance_line->misc }}</td>
 		  </tr>
 	        @endforeach
 	      </tbody>
