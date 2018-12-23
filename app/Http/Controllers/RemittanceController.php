2eb6b1e9577480e8175c161f1677037cf16494e9
Shyam Prasad Sitaula <shyam.sitaula@oit.com.np>
2018-12-23 13:28:52 +0545

    Removing duplicate namespace use statement

--- app/Http/Controllers/RemittanceController.php
+++ app/Http/Controllers/RemittanceController.php
@@ -5,7 +5,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Redirect;
-use Illuminate\Support\Facades\DB;
 
 use App\Http\Requests\SearchFamilyCode;
 use App\Http\Requests\StoreRemit;
