c2e1c7a30cc6b2bf100637142c62f55845e825ed
Shyam Prasad Sitaula <shyam.sitaula@oit.com.np>
2019-03-17 12:11:31 +0545

    Added update ritwik functionality

--- app/Http/Controllers/FamilyController.php
+++ app/Http/Controllers/FamilyController.php
@@ -22,6 +22,41 @@ class FamilyController extends Controller
     }
 
     /**
+     * Extract first, middle and last names from full name.
+     *
+     * @param string fullName
+     *
+     * @return array
+     */
+    public function extractNames($fullName)
+    {
+        $keywords = preg_split("/[\s]+/", $fullName);
+
+	$len = count($keywords);
+
+	$names['first_name'] = null;
+	$names['middle_name'] = null;
+	$names['last_name'] = null;
+
+	/* Has at least one name part */
+	if ($len >= 1) {
+	    $names['first_name'] = $keywords[0];
+	}
+	/* Has at least two name part */
+	if ($len >= 2) {
+	    $names['last_name'] = $keywords[$len-1];
+            /* Has middle names too */
+	    if ($len > 2) {
+	        $middleNames = array_slice($keywords, 1, $len-2);
+	        $names['middle_name'] = implode(' ', $middleNames);
+	    }
+
+	}
+
+	return $names;
+    }
+
+    /**
      * Display a family.
      *
      * @param string famCode
@@ -47,8 +82,19 @@ class FamilyController extends Controller
     {
         $person = Person::find($personId);
 
+	/* Get ritwik list */
+	$ritwiks = Worker::join('person', 'person.person_id', '=', 'worker.person_id')
+	    ->orderBy('person.first_name')
+	    ->get();
+
+	/* Ignore dummy ritwiks */
+        $ritwiks = $ritwiks->except(1);
+        $ritwiks = $ritwiks->except(2);
+        $ritwiks = $ritwiks->except(3);
+
         return view('db.person-update')
-            ->with('person', $person);
+            ->with('person', $person)
+	    ->with('ritwiks', $ritwiks);
     }
 
     /**
@@ -74,37 +120,54 @@ class FamilyController extends Controller
         $person->middle_name = $personName['middle_name'];
         $person->last_name = $personName['last_name'];
 
+	/* Update ritwik if needed */
+	if ($request->has('change_ritwik')) {
+	    $correctRitwik = $request->input('correct_ritwik');
+
+	    /**
+	     * Fetch ritwik from DB, if not found, create one.
+	     */
+	    $ritwikName = $this->extractNames($correctRitwik);
+	    $ritwiks = Worker::all()->where('type', 'R');
+	    
+	    $match = false;
+	    $ritwikId = -1;
+	    foreach ($ritwiks as $ritwik) {
+		$ritwikPerson = Person::find($ritwik->person_id);
+			 // Note: Was a nasty performance bug #pb01
+			 // very slow time to run
+		     // ->where('person_id', $ritwik->person_id)
+			 // ->first();
+
+		if ($ritwikPerson->first_name == $ritwikName['first_name']
+		  &&
+		  $ritwikPerson->middle_name == $ritwikName['middle_name']
+		  &&
+		  $ritwikPerson->last_name == $ritwikName['last_name']) {
+		    $match = true;
+	            echo 'Match found<br />';
+		    $ritwikId = $ritwik->worker_id;
+		    /* Todo: If multiple ritwiks with same name? */
+		    break;
+		}
+	    }
+
+	    /* Ritwik with given name not found */
+	    if (! $match) {
+	        die ('Whoops! Ritwik not found ...');
+	    } else {
+		$oblate = $person->oblate;
+		$oblate->ritwik_id = $ritwikId;
+                $oblate->save();
+	    }
+	}
+
         $person->comment = $comment;
 
         $person->save();
 
         $request->session()->flash('status', 'Success: Person updated!');
         return redirect('/db/family/' . $familyCode);
-    }
-
-    /**
-     * Extract first, middle and last names from full name.
-     *
-     * @param string fullName
-     *
-     * @return array
-     */
-    public function extractNames($fullName)
-    {
-        $keywords = preg_split("/[\s]+/", $fullName);
-
-	    $len = count($keywords);
-
-	    $names['first_name'] = $keywords[0];
-	    $names['last_name'] = $keywords[$len-1];
-	    $names['middle_name'] = null;
-
-	    if ($len > 2) {
-	        $middleNames = array_slice($keywords, 1, $len-2);
-	        $names['middle_name'] = implode(' ', $middleNames);
-	    }
-
-	    return $names;
     }
 
     public function famInp()
--- resources/views/db/person-update.blade.php
+++ resources/views/db/person-update.blade.php
@@ -33,6 +33,20 @@
 	      <!-- Form -->
           <form action="/db/edit/person/p/process" method="post">
 	        {{ csrf_field() }}
+
+	    <!-- Datalist used for ritwik list -->
+	    <datalist id="id_ritwik_list">
+	      @foreach ($ritwiks as $ritwik)
+	        <option 
+	          @if ($ritwik->middle_name != null)
+	            value="{{ $ritwik->first_name }} {{ $ritwik->middle_name }} {{ $ritwik->last_name }}"
+	          @else
+	            value="{{ $ritwik->first_name }} {{ $ritwik->last_name }}"
+	          @endif
+	        >
+	      @endforeach
+	    </datalist>
+
             <table class="table">
               <thead>
               </thead>
@@ -59,6 +73,27 @@
                   <th>Family Code</th>
                   <td>
                     <input type="text" name="family-code" value="{{ $person->oblate->family->family_code}}" readonly />
+                  </td>
+                </tr>
+                <tr>
+                  <th>Ritwik</th>
+                  <td>
+                    <input type="text" name=""
+                      @if ($person->oblate->worker->person->middle_name != null)
+                        value="{{ $person->oblate->worker->person->first_name }} {{ $person->oblate->worker->person->middle_name }} {{ $person->oblate->worker->person->last_name }}"
+                      @else
+                        value="{{ $person->oblate->worker->person->first_name }} {{ $person->oblate->worker->person->last_name }}"
+                      @endif
+		    />
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+		    <input type="checkbox" id="id_change_ritwik" name="change_ritwik">
+		    <label for="id_change_ritwik">Change ritwik</label>
+		  </td>
+                  <td>
+                    <input type="text" name="correct_ritwik" list="id_ritwik_list" />
                   </td>
                 </tr>
                 <tr>
